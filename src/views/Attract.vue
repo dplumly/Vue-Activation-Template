f4347626917782da07373aa857c2f5c9810390f8
Donnie Plumly <donnie.plumly@gmail.com>
2025-10-09 10:30:42 -0700

    Deploy to GitHub Pages

---
+++ src/views/Attract.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="page-content">
+        <!-- Text fade -->
+        <Transition name="fade">
+            <div id="content-container">
+                <div v-if="true" class="fade-group">
+                    <h1>Attract video</h1>
+                    <button class="bottom-button" @click="goNext">Start</button>
+                </div>
+            </div>
+        </Transition>
+    </div>
+</template>
+
+
+
+<script setup>
+import { useRouter } from 'vue-router'
+import { startSession, logClick } from '@/utils/logger.js'
+
+const router = useRouter()
+function goNext() {
+    startSession()
+
+    logClick('Start Button Clicked', 'Attract')
+    router.push('/instructions')
+}
+</script>
+
+<style scoped>
+
+</style>
